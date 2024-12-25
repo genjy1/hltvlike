a768c95135a26a91847ecdd654c150443e998010
Daniil <dmayorov@vend-shop.com>
2024-12-25 16:46:46 +0300

    Добавил загрузку новостей

--- esnhub/src/components/Home.vue
+++ esnhub/src/components/Home.vue
@@ -2,4 +2,20 @@
 
 </template>
 <script setup>
+import { reactive, onMounted } from 'vue';
+import axios from 'axios';
+
+const news = reactive({})
+
+const getNews = async() => {
+  const request = await axios.get('http://localhost:5000/api/news')
+
+  Object.assign(news, request.data)
+}
+
+console.log(news);
+
+
+onMounted(getNews)
+
 </script>
--- esnhub/src/router/index.js
+++ esnhub/src/router/index.js
@@ -1,3 +1,4 @@
+import Home from '@/components/Home.vue'
 import { createRouter, createWebHistory } from 'vue-router'
 
 const router = createRouter({
@@ -6,6 +7,7 @@ const router = createRouter({
     {
       path: '/',
       name: 'home',
+      component: Home
     },
   ],
 })
