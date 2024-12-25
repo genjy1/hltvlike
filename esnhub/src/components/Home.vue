32b25a17961418b8478e6faa6655d72e20fa51f7
Daniil <dmayorov@vend-shop.com>
2024-12-25 16:53:24 +0300

    Добавил рендер

--- esnhub/src/components/Home.vue
+++ esnhub/src/components/Home.vue
@@ -1,19 +1,20 @@
 <template>
-
+  <div v-for="article in news" class="container mx-auto my-0 w-4/5">
+    <h2>{{ article.title }}</h2>
+  </div>
 </template>
 <script setup>
-import { reactive, onMounted } from 'vue';
+import { reactive, onMounted, ref } from 'vue';
 import axios from 'axios';
 
-const news = reactive({})
+let news = ref({})
 
 const getNews = async() => {
   const request = await axios.get('http://localhost:5000/api/news')
+  const requestedNews = request.data.news
 
-  Object.assign(news, request.data)
+  news.value = requestedNews
 }
-
-console.log(news);
 
 
 onMounted(getNews)
