d25ae1fff1cd297005df655ee54688bcad63bebd
Daniil <dmayorov@vend-shop.com>
2024-12-25 16:36:22 +0300

    init

---
+++ esnhub/eslint.config.js
@@ -0,0 +1,27 @@
+import js from '@eslint/js'
+import pluginVue from 'eslint-plugin-vue'
+import oxlint from 'eslint-plugin-oxlint'
+import skipFormatting from '@vue/eslint-config-prettier/skip-formatting'
+
+export default [
+  {
+    name: 'app/files-to-lint',
+    files: ['**/*.{js,mjs,jsx,vue}'],
+  },
+
+  {
+    name: 'app/files-to-ignore',
+    ignores: ['**/dist/**', '**/dist-ssr/**', '**/coverage/**'],
+  },
+
+  {
+    "vue/multi-word-component-names": ["error", {
+      "ignores": []
+    }]
+  },
+
+  js.configs.recommended,
+  ...pluginVue.configs['flat/essential'],
+  oxlint.configs['flat/recommended'],
+  skipFormatting,
+]
---
+++ esnhub/src/components/HeaderComponent.vue
@@ -0,0 +1,39 @@
+<template>
+  <header>
+    <div class="header-container w-4/5 mx-auto my-0 py-2 flex justify-between">
+      <div class="logo-wrapper">
+        <RouterLink to="/" class="font-bold text-lg bg-clip-text text-transparent bg-gradient-to-r from-green-400 to-blue-500">
+          ESNHub
+        </RouterLink>
+    </div>
+    <nav class="header-nav">
+      <ul class="nav-list flex justify-between gap-8">
+        <li>
+          <RouterLink to="#">Team Ranking</RouterLink>
+        </li>
+        <li>
+          <RouterLink to="#">Player Ranking</RouterLink>
+        </li>
+        <li>
+          <RouterLink to="#">Events</RouterLink>
+        </li>
+      </ul>
+    </nav>
+    </div>
+  </header>
+</template>
+<script setup></script>
+<style scoped>
+ a:hover:not(.logo){
+    text-decoration: underline 2px;
+    text-underline-offset: 16px;
+ }
+
+
+ .logo:hover{
+  -webkit-text-fill-color:transparent;
+  background-clip: text;
+  -webkit-background-clip:text;
+  background: -webkit-linear-gradient(315deg,#42d392 25%,#647eff)
+ }
+</style>
